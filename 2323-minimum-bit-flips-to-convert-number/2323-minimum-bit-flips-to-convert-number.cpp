8c4628c189fe4d6fec50065d3915aa25e26a49f7
Himagiri Nandan <118928552+HimagiriNandan@users.noreply.github.com>
2025-07-07 19:18:50 +0530

    Time: 0 ms (100%), Space: 7.9 MB (60.01%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int minBitFlips(int start, int goal) {
         if(start == goal) return 0;
-        int steps = 0;
-        while(start > 0 || goal > 0){
-            if((start & 1) != (goal & 1)){
-                steps++;
-            }
-            start = start >> 1;
-            goal = goal >> 1;
+        int ans = start ^ goal;
+        int cnt = 0;
+        while(ans != 0){
+            ans = (ans & ans - 1);
+            cnt++;
         }
-        return steps;
+        return cnt;
     }
 };
