2b60ddb190b7afff86ecaad67722d159cdfb953e
Himagiri Nandan <118928552+HimagiriNandan@users.noreply.github.com>
2025-07-18 11:42:35 +0530

    Time: 206 ms (23.8%), Space: 14 MB (27.51%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int f(int i, int j, vector<int>& nums, vector<vector<int>>& dp){
+    int f(vector<int>& nums, int i, int j, vector<vector<int>>& dp){
         if(i > j) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        int maxi = INT_MIN;
-        for(int ind = i; ind <= j; ind++){
-            int coins = (nums[i - 1] * nums[ind] * nums[j + 1]) + f(i, ind - 1, nums, dp) + 
-            f(ind + 1, j, nums, dp);
-            maxi = max(maxi, coins);
+        int res = INT_MIN;
+        for(int k = i; k <= j; k++){
+            int ans = (nums[i - 1] * nums[k] * nums[j + 1]) + f(nums, i, k - 1, dp) + f(nums, k + 1, j, dp);
+            res = max(res, ans);
         }
-        return dp[i][j] = maxi;
+        return dp[i][j] = res;
     }
     int maxCoins(vector<int>& nums) {
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
         int n = nums.size();
         vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
-        return f(1, n - 2, nums, dp);
+        return f(nums, 1, nums.size() - 2, dp);
     }
 };
