164fe87c1c94ec9aee1508c63b41571305021158
Himagiri Nandan <118928552+HimagiriNandan@users.noreply.github.com>
2026-06-18 12:02:27 +0530

    Time: 4 ms (62.69%), Space: 39.9 MB (43.02%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] >= 'a' && s[i] <= 'z') {
+                res.push_back(s[i]);
+            } else if(s[i] == '*'){
+                if(!res.empty()){
+                    res.pop_back();
+                }
+            } else if(s[i] == '#'){
+                res += res;
+            } else if (s[i] == '%') {
+                reverse(res.begin(), res.end());
+            }
+        }
+        return res;
+    }
+};
